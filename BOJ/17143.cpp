903a08e1126877b2b582887ca6a3f79c4ba87a5b
raiders032 <nameks@naver.com>
2020-08-25 09:07:23 +0900

    17143. 낚시왕 풀이중 2

--- BOJ/17143.cpp
+++ BOJ/17143.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<math.h>
 using namespace std;
 
 typedef struct _shark {
@@ -12,8 +13,8 @@ typedef struct _shark {
 
 int board[101][101];
 int R, C, M;
-//int dx[5] = {0,-1,1,0,0};
-//int dy[5] = {0,0,0,1,-1};
+int dx[5] = {0,-1,1,0,0};
+int dy[5] = {0,0,0,1,-1};
 vector <shark> shark_list;
 
 void catch_shark(int col) {
@@ -21,7 +22,7 @@ void catch_shark(int col) {
 	int dist = 200;
 	int size = shark_list.size();
 	for (int i = 0; i < size; i++) {
-		if (shark_list[i].c != col) continue;
+		if (shark_list[i].c != col) continue; 
 		if (shark_list[i].r < dist) {
 			idx = i;
 			dist = shark_list[i].r;
@@ -31,7 +32,41 @@ void catch_shark(int col) {
 }
 
 void move_shark(shark shark) {
-
+	int nr, nc, s= shark.s;
+	switch (shark.d){
+	case 1:
+	case 2:
+		while (s) {
+			nr = shark.r + dx[shark.d];
+			if (0 >= nr) {
+				shark.d = 2;
+				nr = shark.r + dx[shark.d];
+			}
+			else if (nr > R) {
+				shark.d = 1;
+				nr = shark.r + dx[shark.d];
+			}
+			shark.r = nr;
+			s--;
+		}	
+		break;
+	case 3:
+	case 4:
+		while (s) {
+			nc = shark.c + dy[shark.d];
+			if (0 >= nc) {
+				shark.d = 3;
+				nc = shark.c + dy[shark.d];
+			}
+			else if (nc > C) {
+				shark.d = 4;
+				nc = shark.c + dy[shark.d];
+			}
+			shark.c = nc;
+			s--;
+		}
+		break;
+	}
 }
 
 void move() {
@@ -44,12 +79,22 @@ void move() {
 int main(void) {
 	int r, c, s, d, z;
 	cin >> R >> C >> M;
-	
+
 	for (int i = 0; i < M; i++) {
 		cin >> r >> c >> s >> d >> z;
+		switch (d)
+		{
+		case 1:
+		case 2:
+			s = s % ((r - 1) + (R - 1) + (R - r));
+			break;
+		case 3:
+		case 4:
+			s = s % ((c - 1) + (C - 1) + (C - c));
+			break;
+		}
 		shark_list.push_back({ r,c,s,d,z });
 	}
-
 	for (int col = 1; col <= c; col++) {
 		catch_shark(col);
 		move();
