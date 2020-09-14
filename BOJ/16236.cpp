a919956784b1e38bbc00caf0b489100f82f3ba6a
raiders032 <nameks@naver.com>
2020-09-14 23:44:40 +0900

    16236. 아기 상어 commit1

--- BOJ/16236.cpp
+++ BOJ/16236.cpp
@@ -1,78 +1,112 @@
+/*
+	bfs
+	1. 물고기가 있는지 확인
+	2. 1마리면 그 물고기를 먹으러 간다.
+	3. 1마리보다 많이면 거리가 가까운 물고기를 먹는다. 
+
+*/
 #include<iostream>
-#include<queue>
 #include<string.h>
+#include<queue>
+#include<algorithm>
 
 using namespace std;
-int N, ans;
-int board[20][20];
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,-1,0,1};
+
 int shark[2];
+int board[20][20];
+int visited[20][20];
+int N;
+int dx[4] = { 0,0,1,-1 };
+int dy[4] = { 1,-1,0,0 };
 
-bool move(int& x, int& y) {
-	int visited[20][20];
-	bool find = false;
+bool noFish() {
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			if (board[i][j] > 0)
+				return false;
+		}
+	}
+	return true;
+}
+
+bool cmp(const pair<int, int>& a, const pair<int, int>& b){
+	if (a.first == b.first)
+		return a.second < b.second;
+	return a.first < b.first;
+}
+
+vector<pair<int, int>> findFish(int x, int y, int& minDist) {
+	vector<pair<int, int>> fish;
 	queue <pair<int, int>> q;
-	memset(visited, -1, sizeof(visited));
-	visited[x][y] = board[x][y] = 0;
+	visited[x][y] = 0;
 	q.push(make_pair(x, y));
-
-	while (!q.empty() && !find) {
+	while (!q.empty()) {
 		x = q.front().first;
 		y = q.front().second;
 		q.pop();
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= N)
+		if (visited[x][y] >= minDist) break;
+		for (int i = 0; i < 4; i++) {
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= N || ny <0 || ny >= N)
 				continue;
-
-			if (visited[nx][ny] != -1 || shark[0] < board[nx][ny])
+			if (shark[0] < board[nx][ny] || visited[nx][ny] != -1)
 				continue;
-
-			if (board[nx][ny] < shark[0] && board[nx][ny] > 0) {
-				if (shark[0] == shark[1] + 1) {
-					shark[0]++;
-					shark[1] = 0;
-				}
-				else {
-					shark[1]++;
-				}
-				ans += visited[x][y] + 1;
-				x = nx;
-				y = ny;
-				find = true;
-				break;
-			}
-
 			visited[nx][ny] = visited[x][y] + 1;
+			if ( 0 < board[nx][ny]  && board[nx][ny] < shark[0]) {
+				fish.push_back(make_pair(nx, ny));
+				minDist = min( minDist, visited[x][y] + 1);
+			}
 			q.push(make_pair(nx, ny));
 		}
 	}
-	return find;
+	return fish;
+}
+
+
+bool moveAndEat(int& x, int& y, int& ans) {
+	int minDist = 1000;
+	vector<pair<int, int>> fish = findFish(x, y, minDist);
+	
+	if (fish.size() == 0) {
+		return false;
+	}
+
+	sort(fish.begin(), fish.end(), cmp);
+	x = fish.front().first;
+	y = fish.front().second;
+	ans += minDist;
+	board[x][y] = 0;
+
+	if (shark[0] == shark[1] + 1) {
+		shark[0]++;
+		shark[1] = 0;
+	}
+	else
+		shark[1]++;
+	return true;
 }
 
 int main(void) {
-
-	int x, y, tmp;
-
-	cin >> N;
+	int x, y, t = 0 ;
+	int ans = 0;
 	shark[0] = 2;
-
+	shark[1] = 0;
+	cin >> N;
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			cin >> tmp;
-			board[i][j] = tmp;
-			if (tmp == 9) {
+			cin >> board[i][j];
+			if (board[i][j] == 9) {
 				x = i;
 				y = j;
+				board[i][j] = 0;
 			}
 		}
 	}
 
-	while (move(x, y)) {
-	}
+	do {
+		memset(visited, -1, sizeof(visited));
+	}while (!noFish() && moveAndEat(x, y, ans));
 
 	cout << ans;
 }
