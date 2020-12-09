cc57c9e29f4d3fd9682afe68cba87a78b5693660
raiders032 <nameks@naver.com>
2020-12-09 13:30:55 +0900

    에디터 스택이용

--- BOJ/1406.py
+++ BOJ/1406.py
@@ -1,21 +1,20 @@
 import sys
-string = list(sys.stdin.readline().strip())
-cur = len(string)
+left = list(sys.stdin.readline().strip())
+right = []
 N = int(sys.stdin.readline().strip())
 
 for _ in range(N):
     cmd = sys.stdin.readline().strip()
     if cmd.startswith('P'):
         cmd, ch = cmd.split()
-        string.insert(cur, ch)
-        cur += 1
-    elif cmd.startswith('D'):
-        cur += 1 if cur <= len(string) else 0
+        left.append(ch)
     elif cmd.startswith('L'):
-        cur -= 1 if cur > 0 else 0
+        if left:
+            right.append(left.pop())
+    elif cmd.startswith('D'):
+        if right:
+            left.append(right.pop())
     elif cmd.startswith('B'):
-        if cur == 0:
-            continue
-        string.pop(cur-1)
-        cur -= 1
-print(''.join(string))
+        if left:
+            left.pop()
+print(''.join(left + right[::-1]))
