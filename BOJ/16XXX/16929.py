746d5efc421e7a9e9943f8db31f43c7fe24e3a72
raiders032 <nameks@naver.com>
2021-03-07 15:40:44 +0900

    16929번 Two Dots 골드4 깊이우선탐색,그래프이론,그래프탐색 풀이2.140ms

--- BOJ/16XXX/16929.py
+++ BOJ/16XXX/16929.py
@@ -1,45 +1,51 @@
-'''
+"""
 https://www.acmicpc.net/problem/16929
 16929번 Two Dots
 골드4
 깊이우선탐색,그래프이론,그래프탐색
-풀이1. 실패
-'''
+풀이2.140ms
+"""
+import sys
+
+
+def is_cycle(level, start_x, start_y, current_x, current_y):
+    for direction in range(4):
+        next_x = current_x + dx[direction]
+        next_y = current_y + dy[direction]
+
+        if next_x < 0 or next_x >= N or next_y < 0 or next_y >= M:
+            continue
+
+        if board[current_x][current_y] != board[next_x][next_y]:
+            continue
+
+        if level >= 3 and next_x == start_x and next_y == start_y:
+            return True
+
+        if visited[next_x][next_y]:
+            continue
+
+        visited[next_x][next_y] = True
+        if is_cycle(level + 1, start_x, start_y, next_x, next_y):
+            return True
+        visited[next_x][next_y] = False
+
+    return False
+
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+board = [list(input().rstrip()) for _ in range(N)]
+visited = [[False] * M for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-N, M = map(int, input().split())
-board = [list(input()) for _ in range(N)]
-visited = [[False] * M for _ in range(N)]
-
-
-def dfs(sx, sy, x, y, dist):
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
-            continue
-        if sx == nx and sy == ny and dist > 1:
-            print("Yes")
-            exit(0)
-        if board[x][y] == board[nx][ny] and visited[nx][ny] == False:
-            visited[nx][ny] = True
-            dfs(sx, sy, nx, ny, dist + 1)
-            visited[nx][ny] = False
-
 
 for x in range(N):
     for y in range(M):
-        cnt = 0
-        for dir in range(4):
-            nx = x + dx[dir]
-            ny = y + dy[dir]
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
-            if board[x][y] == board[nx][ny]:
-                cnt += 1
-        if cnt < 2:
-            continue
         visited[x][y] = True
-        dfs(x, y, x, y, 0)
+        if is_cycle(0, x, y, x , y):
+            print('Yes')
+            exit(0)
         visited[x][y] = False
-print("No")
+
+print('No')
