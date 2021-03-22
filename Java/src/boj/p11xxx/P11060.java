1a8179538487c91f8899b175b7f4a8291a0f3de2
raiders032 <nameks@naver.com>
2021-03-22 17:24:50 +0900

    11060.점프 점프 실버2 DPx 풀이1.304ms

---
+++ Java/src/boj/p11xxx/P11060.java
@@ -0,0 +1,44 @@
+package boj.p11xxx;
+
+/*
+https://www.acmicpc.net/problem/11060
+11060.점프 점프
+실버2
+DPx
+풀이1.304ms
+*/
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P11060 {
+    static int n;
+    static int[] array;
+    static int[] dp;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        array = new int[n];
+        dp = new int[n];
+        Arrays.fill(dp, 10000);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        for (int index = 0; index < n; index++) {
+            for (int next = index + 1; next < n && next <= index + array[index]; next++) {
+                dp[next] = Math.min(dp[next], dp[index] + 1);
+            }
+        }
+
+        if(dp[n-1] == 10000)
+            System.out.println(-1);
+        else
+            System.out.println(dp[n - 1]);
+
+    }
+
+}
