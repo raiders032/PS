0be9dce6b13bf368f8c81415f8630951c5b0cb4f
raiders032 <nameks@naver.com>
2021-04-22 10:54:05 +0900

    큰 수 만들기 탐욕법 풀이2

--- python/Programmers/큰 수 만들기.py
+++ python/Programmers/큰 수 만들기.py
@@ -2,25 +2,30 @@
 https://programmers.co.kr/learn/courses/30/lessons/42883
 큰 수 만들기
 탐욕법
-풀이1.시간초과
+풀이2
 """
 
 
 def solution(number, k):
     count = 0
+    length = len(number)
+    index = 0
 
     while count < k:
-        for index in range(len(number) - 1):
-            if int(number[index]) >= int(number[index + 1]):
+        for i in range(index, length - 1):
+            if int(number[i]) >= int(number[i + 1]):
                 continue
             else:
                 count += 1
-                number = number[0:index] + number[index + 1: len(number)]
+                index = i - 1 if i - 1 >= 0 else 0
+                number = number[0:i] + number[i + 1: length]
+                length -= 1
                 break
         else:
             number = number[0: -(k - count)]
             break
-    return str(number)
+
+    return number
 
 
-print(solution("321", 1))
+print(solution("123456", 1))
