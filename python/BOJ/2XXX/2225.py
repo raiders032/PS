47c6298c9947d05eed4325f6b7b296f7974983f1
raiders032 <nameks@naver.com>
2021-05-04 18:27:25 +0900

    BOJ 2225.합분해 골드5 다이나믹프로그래밍 풀이2.1024ms

--- python/BOJ/2XXX/2225.py
+++ python/BOJ/2XXX/2225.py
@@ -3,23 +3,19 @@ https://www.acmicpc.net/problem/2225
 2225.합분해
 골드5
 다이나믹프로그래밍
-풀이1.1728ms
+풀이2.1024ms
 """
+import sys
 
-
+input = sys.stdin.readline
 N, K = map(int, input().split())
-dp = [[0] * (K+1) for _ in range(N + 1)]
+dp = [[1] * (N + 1) for _ in range(K + 1)]
 
-for i in range(0, N + 1):
-    dp[i][1] = 1
+for i in range(2, K + 1):
+    for j in range(1, N + 1):
+        dp[i][j] = 0
+        for k in range(j + 1):
+            dp[i][j] += dp[i - 1][j - k]
+        dp[i][j] = dp[i][j] % 1000000000
 
-for i in range(1, K + 1):
-    dp[0][i] = 1
-
-for select_count in range(2, K + 1):
-    for num in range(1, N + 1):
-        for last_selected_num in range(0, num + 1):
-            dp[num][select_count] += dp[num - last_selected_num][select_count - 1]
-            dp[num][select_count] %= 1000000000
-
-print(dp[N][K])
+print(dp[K][N])
