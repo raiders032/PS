9abf52bcb1a23ca8f3f86e71150f954ac547c301
raiders032 <nameks@naver.com>
2021-07-31 14:04:21 +0900

    BOJ 11724.연결 요소의 개수 실버2 BFS, DFS, 그래프이론, 그래프탐색 풀이1.948ms

--- python/BOJ/11XXX/11724.py
+++ python/BOJ/11XXX/11724.py
@@ -2,30 +2,32 @@
 11724.연결 요소의 개수
 실버2
 BFS, DFS, 그래프이론, 그래프탐색
+풀이1.948ms
 """
 import sys
-
 sys.setrecursionlimit(100000)
+input = sys.stdin.readline
 
 def dfs(v):
     visited[v] = True
     for next_v in graph[v]:
-        if visited[next_v]:
-            continue
-        dfs(next_v)
+        if not visited[next_v]:
+            dfs(next_v)
 
 
-input = sys.stdin.readline
 N, M = map(int, input().split())
-graph = [[] for _ in range(N)]
-visited = [False] * N
-cnt = 0
+graph = [list() for _ in range(N + 1)]
+visited = [False] * (N + 1)
+count = 0
+
 for _ in range(M):
-    v1, v2 = map(int, input().split())
-    graph[v1 - 1].append(v2 - 1)
-    graph[v2 - 1].append(v1 - 1)
-for i in range(N):
+    v, w = map(int, input().split())
+    graph[v].append(w)
+    graph[w].append(v)
+
+for i in range(1, N + 1):
     if not visited[i]:
-        cnt += 1
+        count += 1
         dfs(i)
-print(cnt)
+
+print(count)
