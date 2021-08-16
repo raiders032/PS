217866ab0545ef8f6a41259036f17db26414debc
raiders032 <nameks@naver.com>
2021-08-17 08:08:32 +0900

    BOJ https://www.acmicpc.net/problem/5397 5397.키로거 실버3 자료구조, 덱, 스택 풀이3.1464ms

--- python/BOJ/5XXX/5397.py
+++ python/BOJ/5XXX/5397.py
@@ -3,29 +3,23 @@ https://www.acmicpc.net/problem/5397
 5397.키로거
 실버3
 자료구조, 덱, 스택
-풀이2.1280ms
+풀이3.1464ms
 """
-import sys
-
-input = sys.stdin.readline
-N = int(input())
-for _ in range(N):
-    left = []
-    right = []
-    for char in list(input().rstrip()):
-        if char == '<':
-            if not left:
-                continue
-            right.append(left.pop())
-        elif char == '>':
-            if not right:
-                continue
-            left.append(right.pop())
-        elif char == '-':
-            if not left:
-                continue
-            left.pop()
+from collections import deque
+T = int(input())
+for _ in range(T):
+    left = deque()
+    right = deque()
+    for ch in input().rstrip():
+        if ch == '<':
+            if left:
+                right.appendleft(left.pop())
+        elif ch == '>':
+            if right:
+                left.append(right.popleft())
+        elif ch == '-':
+            if left:
+                left.pop()
         else:
-            left.append(char)
-
-    print(''.join(left + right[::-1]))
+            left.append(ch)
+    print(''.join(left) + ''.join(right))
