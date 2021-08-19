f91dc3d60428559721a186d0baa4ab4c45d13c5b
raiders032 <nameks@naver.com>
2021-08-20 08:58:16 +0900

    BOJ https://www.acmicpc.net/problem/9663 9663.N-Queen 골드5 풀이2.5612ms(PyPy3)

--- python/BOJ/9XXX/9663.py
+++ python/BOJ/9XXX/9663.py
@@ -2,46 +2,31 @@
 https://www.acmicpc.net/problem/9663
 9663.N-Queen
 골드5
-풀이1.5844ms(PyPy3)
+풀이2.5612ms(PyPy3)
 """
 
 
-def dfs(x):
-    global ans
-
-    if x == N - 1:
-        ans += 1
+def solve(row):
+    global answer
+    if row == N:
+        answer += 1
         return
 
-    row = x + 1
-
     for col in range(N):
-        if col_visited[col] or right_dia_visited[row - col] or left_dia_visited[row + col]:
+        if col_visited[col]:
             continue
-        col_visited[col] = True
-        right_dia_visited[row - col] = True
-        left_dia_visited[row + col] = True
-        dfs(row)
-        col_visited[col] = False
-        right_dia_visited[row - col] = False
-        left_dia_visited[row + col] = False
+        if left_diag_visited[row - col] or right_diag_visited[row + col]:
+            continue
+        col_visited[col] = left_diag_visited[row - col] = right_diag_visited[row + col] = True
+        solve(row + 1)
+        col_visited[col] = left_diag_visited[row - col] = right_diag_visited[row + col] = False
 
 
+answer = 0
 N = int(input())
 col_visited = [False] * N
-left_dia_visited = [False] * (2 * N - 1)
-right_dia_visited = [False] * (2 * N - 1)
-ans = 0
+left_diag_visited = [False] * (2 * N - 1)
+right_diag_visited = [False] * (2 * N - 1)
 
-for col in range(N):
-    col_visited[col] = True
-    right_dia_visited[-col] = True
-    left_dia_visited[col] = True
-
-    dfs(0)
-
-    col_visited[col] = False
-    right_dia_visited[-col] = False
-    left_dia_visited[col] = False
-
-print(ans)
+solve(0)
+print(answer)
