7dc96e1f1cac4d6609b3007dfddedabf412ff4e5
raiders032 <nameks@naver.com>
2021-08-25 08:05:37 +0900

    BOJ https://www.acmicpc.net/problem/1715 1715.카드 정렬하기 골드4 풀이1.228ms

---
+++ python/BOJ/1XXX/1715.py
@@ -0,0 +1,21 @@
+"""
+https://www.acmicpc.net/problem/1715
+1715.카드 정렬하기
+골드4
+풀이1.228ms
+"""
+import sys, heapq
+
+input = sys.stdin.readline
+N = int(input())
+cards = []
+ans = 0
+for _ in range(N):
+    heapq.heappush(cards, int(input()))
+
+while len(cards) > 1:
+    count = heapq.heappop(cards) + heapq.heappop(cards)
+    ans += count
+    heapq.heappush(cards, count)
+
+print(ans)
