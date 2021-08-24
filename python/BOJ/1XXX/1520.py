93c6ccc1b64b8483affb60a59e669e8cff5853b0
raiders032 <nameks@naver.com>
2021-08-25 07:55:17 +0900

    BOJ https://www.acmicpc.net/problem/1520 1520.내리막 길 골드4 풀이1.192ms

--- python/BOJ/1XXX/1520.py
+++ python/BOJ/1XXX/1520.py
@@ -7,30 +7,28 @@ https://www.acmicpc.net/problem/1520
 import sys
 sys.setrecursionlimit(10**6)
 
-
 def solve(x, y):
     if x == N - 1 and y == M - 1:
         return 1
 
-    if cache[x][y] == -1:
-        cache[x][y] = 0
-
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+    if visited[x][y] == -1:
+        visited[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
             if board[x][y] <= board[nx][ny]:
                 continue
-            cache[x][y] += solve(nx, ny)
+            visited[x][y] += solve(nx, ny)
 
-    return cache[x][y]
+    return visited[x][y]
 
 
 input = sys.stdin.readline
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-cache = [[-1] * M for _ in range(N)]
+visited = [[-1] * M for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 print(solve(0, 0))
