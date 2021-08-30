35115baaf29a79580544d08f7935d823a6e7a03f
raiders032 <nameks@naver.com>
2021-08-30 10:32:41 +0900

    BOJ https://www.acmicpc.net/problem/11659 11659.구간 합 구하기 4 실버3 풀이1.328ms

---
+++ python/BOJ/11XXX/11659.py
@@ -0,0 +1,21 @@
+"""
+https://www.acmicpc.net/problem/11659
+11659.구간 합 구하기 4
+실버3
+풀이1.328ms
+"""
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+nums = list(map(int, input().split()))
+sums = [nums[0]] + [0] * (N - 1)
+for i in range(1, N):
+    sums[i] = sums[i - 1] + nums[i]
+
+for _ in range(M):
+    start, end = map(lambda x: int(x) - 1, input().split())
+    answer = sums[end]
+    if start:
+        answer -= sums[start - 1]
+    print(answer)
