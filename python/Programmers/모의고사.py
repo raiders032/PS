67e787d885a1adcec0011a6fdd95cf66c314f2a1
raiders032 <nameks@naver.com>
2021-08-31 15:42:28 +0900

    programmers https://programmers.co.kr/learn/courses/30/lessons/42840 모의고사 레벨1 풀이2

--- python/Programmers/모의고사.py
+++ python/Programmers/모의고사.py
@@ -2,32 +2,26 @@
 https://programmers.co.kr/learn/courses/30/lessons/42840
 모의고사
 레벨1
-완전탐색
-풀이1
+풀이2
 """
 
-def solution(answers):
-    scores = [0, 0, 0]
-    patterns_1 = [1, 2, 3, 4, 5]
-    patterns_2 = [2, 1, 2, 3, 2, 4, 2, 5]
-    patterns_3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 
-    for index, answer in enumerate(answers):
-        scores[0] += 1 if patterns_1[index % 5] == answer else 0
-        scores[1] += 1 if patterns_2[index % 8] == answer else 0
-        scores[2] += 1 if patterns_3[index % 10] == answer else 0
+def solution(array):
+    answers = [[1, 2, 3, 4, 5], [2, 1, 2, 3, 2, 4, 2, 5], [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    result = []
+    max_count = 0
 
-    res = []
-    max_score = max(scores)
     for i in range(3):
-        if max_score == scores[i]:
-            res.append(i + 1)
-    return res
+        count = 0
+        for index, answer in enumerate(array):
+            if answers[i][index % len(answers[i])] == answer:
+                count += 1
 
-print(solution([1, 2, 3, 4, 5]))
+        if max_count <= count:
+            if max_count == count:
+                result.append(i + 1)
+            else:
+                result = [i + 1]
+            max_count = count
 
-"""
-answers	return
-[1,2,3,4,5]	[1]
-[1,3,2,4,2]	[1,2,3]
-"""
+    return result
