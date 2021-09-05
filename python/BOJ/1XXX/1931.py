4aa192fc12e64c8cf99664c6f20e71b1091d9a6e
raiders032 <nameks@naver.com>
2021-09-05 20:41:41 +0900

    BOJ https://www.acmicpc.net/problem/1931 1931.회의실 배정 실버2 풀이2.324ms

--- python/BOJ/1XXX/1931.py
+++ python/BOJ/1XXX/1931.py
@@ -2,25 +2,26 @@
 https://www.acmicpc.net/problem/1931
 1931.회의실 배정
 실버2
-풀이1.
+풀이2.324ms
 """
 import sys
 
 input = sys.stdin.readline
-meetings = list()
-answer = 0
-last_end = 0
 
-for _ in range(int(input())):
+N = int(input())
+meetings = []
+answer = 0
+
+for _ in range(N):
     start, end = map(int, input().split())
     meetings.append((start, end))
 
 meetings.sort(key=lambda x: (x[1], x[0]))
 
-for meeting in meetings:
-    if meeting[0] < last_end:
-        continue
-    last_end = meeting[1]
-    answer += 1
+last_end = 0
+for (start, end) in meetings:
+    if last_end <= start:
+        answer += 1
+        last_end = end
 
 print(answer)
