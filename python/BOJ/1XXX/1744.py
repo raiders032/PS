03f32f36c3cf7947bb7279a2f12c09c4c1702589
raiders032 <nameks@naver.com>
2021-09-05 20:41:32 +0900

    BOJ https://www.acmicpc.net/problem/1744 1744.수 묶기 골드4 풀이2.88ms

--- python/BOJ/1XXX/1744.py
+++ python/BOJ/1XXX/1744.py
@@ -2,19 +2,18 @@
 https://www.acmicpc.net/problem/1744
 1744.수 묶기
 골드4
-풀이1.84ms
+풀이2.88ms
 """
 import sys
 import heapq
 
 input = sys.stdin.readline
-N = int(input())
-nums = [int(input()) for _ in range(N)]
 positive_nums = list()
 negative_nums = list()
-ans = 0
+answer = 0
 
-for num in nums:
+for _ in range(int(input())):
+    num = int(input())
     if num > 0:
         heapq.heappush(positive_nums, -num)
     else:
@@ -25,17 +24,17 @@ while positive_nums:
 
     if positive_nums:
         num2 = -heapq.heappop(positive_nums)
-        ans += num1 * num2 if num2 != 1 else num1 + num2
+        answer += num1 * num2 if num2 != 1 else num1 + num2
     else:
-        ans += num1
+        answer += num1
 
 while negative_nums:
     num1 = heapq.heappop(negative_nums)
-    num2 = 1
 
     if negative_nums:
         num2 = heapq.heappop(negative_nums)
+        answer += num1 * num2
+    else:
+        answer += num1
 
-    ans += num1 * num2
-
-print(ans)
+print(answer)
