8b4a2b7ab7a945b61ff291e95da71d85d29029aa
raiders032 <nameks@naver.com>
2021-09-15 11:22:46 +0900

    BOJ https://www.acmicpc.net/problem/1535 1535.안녕 실버2 풀이2.64ms

--- python/BOJ/1XXX/1535.py
+++ python/BOJ/1XXX/1535.py
@@ -2,17 +2,16 @@
 https://www.acmicpc.net/problem/1535
 1535.안녕
 실버2
-풀이1.84ms
+풀이2.64ms
 """
 N = int(input())
 W = [0] + list(map(int, input().split()))
-V = [0] + list(map(int, input().split()))
+P = [0] + list(map(int, input().split()))
 dp = [[0] * 101 for _ in range(N + 1)]
 
 for i in range(1, N + 1):
     for j in range(1, 101):
         dp[i][j] = dp[i - 1][j]
-        if W[i] < j:
-            dp[i][j] = max(dp[i][j], dp[i - 1][j - W[i]] + V[i])
-
+        if j - W[i] > 0:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - W[i]] + P[i])
 print(dp[N][100])
