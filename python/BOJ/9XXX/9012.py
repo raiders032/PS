66db7ce60363cac6344696454f83453ff597f1b5
raiders032 <nameks@naver.com>
2021-09-16 10:32:47 +0900

    BOJ https://www.acmicpc.net/problem/9012 9012.괄호 실버4 풀이1.88ms

--- python/BOJ/9XXX/9012.py
+++ python/BOJ/9XXX/9012.py
@@ -4,16 +4,16 @@ https://www.acmicpc.net/problem/9012
 실버4
 풀이1.88ms
 """
-for _ in range(int(input())):
+N = int(input())
+for _ in range(N):
     count = 0
-    for ch in list(input()):
+    for ch in input():
         if ch == '(':
             count += 1
         else:
             count -= 1
             if count < 0:
                 break
-
     if count:
         print("NO")
     else:
