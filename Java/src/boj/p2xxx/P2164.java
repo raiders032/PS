8f91e9b130188fc8faf48467238e18902f51c7cf
raiders032 <nameks@naver.com>
2021-09-16 20:28:47 +0900

    BOJ https://www.acmicpc.net/problem/2164     2164.카드2     실버4     풀이2.288ms

--- Java/src/boj/p2xxx/P2164.java
+++ Java/src/boj/p2xxx/P2164.java
@@ -4,28 +4,27 @@ package boj.p2xxx;
 https://www.acmicpc.net/problem/2164
 2164.카드2
 실버4
-풀이1.288ms
+풀이2.288ms
  */
 
-import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class P2164 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-
-        Deque<Integer> deque = new ArrayDeque<>();
+        Deque<Integer> deque = new LinkedList<>();
         for (int i = 1; i <= n; i++) {
             deque.addLast(i);
         }
 
-        while (deque.size() >= 2) {
+        while (deque.size() > 1) {
             deque.pollFirst();
             deque.addLast(deque.pollFirst());
         }
 
-        System.out.println(deque.getFirst());
+        System.out.println(deque.pollLast());
     }
 }
