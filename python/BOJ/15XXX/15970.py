448f1b828777f787e9679d6f532f26ae4a509a62
raiders032 <nameks@naver.com>
2022-06-25 16:13:27 +0900

    https://www.acmicpc.net/problem/15970 15970.화살표 그리기 실버4 풀이1.100ms

---
+++ python/BOJ/15XXX/15970.py
@@ -0,0 +1,25 @@
+"""
+https://www.acmicpc.net/problem/15970
+15970.화살표 그리기
+실버4
+풀이1.100ms
+"""
+import sys
+import collections
+input = sys.stdin.readline
+
+n = int(input())
+color_point = collections.defaultdict(list)
+for _ in range(n):
+    position, color = map(int, input().split())
+    color_point[color].append(position)
+
+answer = 0
+for points in color_point.values():
+    points.sort()
+    for position in range(len(points)):
+        to_left_length = points[position] - points[position - 1] if position else sys.maxsize
+        to_right_length = points[position + 1] - points[position] if position + 1 < len(points) else sys.maxsize
+        answer += min(to_left_length, to_right_length)
+
+print(answer)
