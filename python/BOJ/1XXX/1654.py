f696ac6fa29e188bc13c70af6497aa667c86d375
raiders032 <nameks@naver.com>
2022-06-29 16:41:48 +0900

    https://www.acmicpc.net/problem/1654 1654.랜선 자르기 실버3 풀이2.116ms

--- python/BOJ/1XXX/1654.py
+++ python/BOJ/1XXX/1654.py
@@ -2,7 +2,7 @@
 https://www.acmicpc.net/problem/1654
 1654.랜선 자르기
 실버3
-풀이1.132ms
+풀이2.116ms
 """
 import sys
 
@@ -11,19 +11,18 @@ K, N = map(int, input().split())
 nums = [int(input()) for _ in range(K)]
 left = 1
 right = 2 ** 31 - 1
-ans = 1
+ans = 0
 
 while left <= right:
-    mid = (left + right) // 2
     count = 0
-
+    mid = (left + right) // 2
     for num in nums:
         count += num // mid
 
-    if count < N:
-        right = mid - 1
-    else:
+    if count >= N:
         left = mid + 1
-        ans = mid
+        ans = max(ans, mid)
+    else:
+        right = mid - 1
 
 print(ans)
