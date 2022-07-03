d18a9dd84e41abc32e040c08677513be5efdeb81
raiders032 <nameks@naver.com>
2022-07-03 21:40:28 +0900

    https://www.acmicpc.net/problem/2667 2667.단지번호붙이기 실버1 풀이2.68ms

--- python/BOJ/2XXX/2667.py
+++ python/BOJ/2XXX/2667.py
@@ -1,51 +1,46 @@
 """
+https://www.acmicpc.net/problem/2667
 2667.단지번호붙이기
 실버1
-DFS, BFS, 그래프이론, 그래프탐색
+풀이2.68ms
 """
 import sys
-from collections import deque
+sys.setrecursionlimit(10 ** 6)
 
 
-def get_cnt_by_bfs(x, y):
-    cnt = 1
-    q = deque()
+def get_area(x, y):
     visited[x][y] = True
-    q.append((x, y))
-    while q:
-        cur_x = q[0][0]
-        cur_y = q[0][1]
-        q.popleft()
-        for i in range(4):
-            next_x = cur_x + dx[i]
-            next_y = cur_y + dy[i]
-            if next_x < 0 or next_x >= N or next_y < 0 or next_y >= N:
-                continue
-            if visited[next_x][next_y] or board[next_x][next_y] == 0:
-                continue
-            visited[next_x][next_y] = True
-            q.append((next_x, next_y))
-            cnt += 1
-    return cnt
+    area = 1
 
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-input = sys.stdin.readline
-N = int(input())
-board = []
-visited = [[False] * N for _ in range(N)]
-cnts = []
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
-
-for _ in range(N):
-    row = list(map(int, input().rstrip()))
-    board.append(row)
-for r in range(N):
-    for c in range(N):
-        if visited[r][c] or board[r][c] == 0:
+        if nx < 0 or ny < 0 or nx >= N or ny >= N:
             continue
-        cnts.append(get_cnt_by_bfs(r, c))
-cnts.sort()
-print(len(cnts))
-for x in cnts:
-    print(x)
+        if visited[nx][ny] or not board[nx][ny]:
+            continue
+
+        area += get_area(nx, ny)
+
+    return area
+
+
+N = int(input())
+board = [list(map(int, input().rstrip())) for _ in range(N)]
+# print(board)
+visited = [[False] * N for _ in range(N)]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+
+counts = []
+for i in range(N):
+    for j in range(N):
+        if visited[i][j] or not board[i][j]:
+            continue
+        counts.append(get_area(i, j))
+
+print(len(counts))
+counts.sort()
+for count in counts:
+    print(count)
