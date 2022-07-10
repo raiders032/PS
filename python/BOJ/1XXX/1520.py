5600d02501ddd2e7adb5dec2f67d2e31cf012235
raiders032 <nameks@naver.com>
2022-07-10 15:06:16 +0900

    https://www.acmicpc.net/problem/1520 1520.내리막 길 골드4 풀이2.216ms

--- python/BOJ/1XXX/1520.py
+++ python/BOJ/1XXX/1520.py
@@ -2,33 +2,34 @@
 https://www.acmicpc.net/problem/1520
 1520.내리막 길
 골드4
-풀이1.192ms
+풀이2.216ms
 """
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10 ** 6)
+
 
 def solve(x, y):
-    if x == N - 1 and y == M - 1:
+    if x == 0 and y == 0:
         return 1
 
-    if visited[x][y] == -1:
-        visited[x][y] = 0
+    if cache[x][y] == -1:
+        cache[x][y] = 0
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-            if board[x][y] <= board[nx][ny]:
+            if map[x][y] >= map[nx][ny]:
                 continue
-            visited[x][y] += solve(nx, ny)
+            cache[x][y] += solve(nx, ny)
 
-    return visited[x][y]
+    return cache[x][y]
 
 
 input = sys.stdin.readline
 N, M = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
-visited = [[-1] * M for _ in range(N)]
+map = [list(map(int, input().split())) for _ in range(N)]
+cache = [[-1] * M for _ in range(N)]
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
-print(solve(0, 0))
+print(solve(N - 1, M - 1))
