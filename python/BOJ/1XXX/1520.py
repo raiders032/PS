ab8ee3383155309ac5cb54342aa46172cfc374c0
raiders032 <nameks@naver.com>
2022-07-17 17:31:47 +0900

    https://www.acmicpc.net/problem/1520 1520.내리막 길 골드4 풀이4.220ms

--- python/BOJ/1XXX/1520.py
+++ python/BOJ/1XXX/1520.py
@@ -2,7 +2,7 @@
 https://www.acmicpc.net/problem/1520
 1520.내리막 길
 골드4
-풀이3.204ms
+풀이4.220ms
 """
 import sys
 
@@ -10,32 +10,29 @@ sys.setrecursionlimit(10 ** 6)
 input = sys.stdin.readline
 
 
-def solve(x, y):
-    if cache[x][y] != -1:
-        return cache[x][y]
+def get_route(x, y):
+    if dp[x][y] != -1:
+        return dp[x][y]
 
     if x == 0 and y == 0:
         return 1
 
-    result = 0
+    dp[x][y] = 0
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if nx < 0 or nx >= N or ny < 0 or ny >= M:
+        if nx < 0 or ny < 0 or nx >= N or ny >= M:
             continue
-
-        if board[nx][ny] <= board[x][y]:
+        if board[x][y] >= board[nx][ny]:
             continue
-
-        result += solve(nx, ny)
-
-    cache[x][y] = result
-    return cache[x][y]
+        dp[x][y] += get_route(nx, ny)
+    return dp[x][y]
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-cache = [[-1] * M for _ in range(N)]
+dp = [[-1] * M for _ in range(N)]
 dx = [1, 0, -1, 0]
 dy = [0, -1, 0, 1]
-print(solve(N - 1, M - 1))
+print(get_route(N - 1, M - 1))
+# print(dp)
