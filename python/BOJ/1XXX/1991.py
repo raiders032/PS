23f386fe37ca8043bd2380d393c9c69b5969b50f
raiders032 <nameks@naver.com>
2022-07-17 17:32:04 +0900

    https://www.acmicpc.net/problem/1991 1991.트리 순회 실버1 풀이2.76ms

--- python/BOJ/1XXX/1991.py
+++ python/BOJ/1XXX/1991.py
@@ -2,42 +2,41 @@
 https://www.acmicpc.net/problem/1991
 1991.트리 순회
 실버1
-풀이1.72ms
+풀이2.76ms
 """
 import sys
-
 input = sys.stdin.readline
-N = int(input())
-tree = dict()
 
 
 def pre_order(parent):
     print(parent, end='')
-    if tree[parent][0] != '.':
-        pre_order(tree[parent][0])
-    if tree[parent][1] != '.':
-        pre_order(tree[parent][1])
+    if graph[parent][0] != '.':
+        pre_order(graph[parent][0])
+    if graph[parent][1] != '.':
+        pre_order(graph[parent][1])
 
 
 def in_order(parent):
-    if tree[parent][0] != '.':
-        in_order(tree[parent][0])
+    if graph[parent][0] != '.':
+        in_order(graph[parent][0])
     print(parent, end='')
-    if tree[parent][1] != '.':
-        in_order(tree[parent][1])
+    if graph[parent][1] != '.':
+        in_order(graph[parent][1])
 
 
 def post_order(parent):
-    if tree[parent][0] != '.':
-        post_order(tree[parent][0])
-    if tree[parent][1] != '.':
-        post_order(tree[parent][1])
+    if graph[parent][0] != '.':
+        post_order(graph[parent][0])
+    if graph[parent][1] != '.':
+        post_order(graph[parent][1])
     print(parent, end='')
 
 
-for _ in range(N):
-    parent, left_child, right_child = input().split()
-    tree[parent] = (left_child, right_child)
+n = int(input())
+graph = dict()
+for _ in range(n):
+    parent, left, right = input().rstrip().split()
+    graph[parent] = (left, right)
 
 pre_order('A')
 print()
