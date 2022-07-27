09b4e4590e8096270b00b109b38570a55e22e5e4
raiders032 <nameks@naver.com>
2022-07-27 19:26:43 +0900

    https://www.acmicpc.net/problem/1504 1504.특정한 최단 경로 풀이1.492ms

---
+++ python/BOJ/1XXX/1504.py
@@ -0,0 +1,49 @@
+"""
+https://www.acmicpc.net/problem/1504
+1504.특정한 최단 경로
+풀이1.492ms
+"""
+import sys
+import heapq
+
+input = sys.stdin.readline
+
+
+def get_min_distance(vertex):
+    min_distance = [sys.maxsize] * (N + 1)
+    visited = [False] * (N + 1)
+    min_distance[vertex] = 0
+    min_heap = [(0, vertex)]
+
+    while min_heap:
+        weight, vertex = heapq.heappop(min_heap)
+        if visited[vertex]:
+            continue
+        visited[vertex] = True
+        for next_vertex, next_weight in graph[vertex]:
+            if min_distance[vertex] + next_weight < min_distance[next_vertex]:
+                min_distance[next_vertex] = min_distance[vertex] + next_weight
+                heapq.heappush(min_heap, (min_distance[next_vertex], next_vertex))
+
+    return min_distance
+
+
+N, E = map(int, input().split())
+graph = [list() for _ in range(N + 1)]
+for _ in range(E):
+    v1, v2, w = map(int, input().split())
+    graph[v1].append((v2, w))
+    graph[v2].append((v1, w))
+
+mid1, mid2 = map(int, input().split())
+start = get_min_distance(1)
+intermediate1 = get_min_distance(mid1)
+intermediate2 = get_min_distance(mid2)
+
+answer = sys.maxsize
+if start[mid1] != sys.maxsize and intermediate1[mid2] != sys.maxsize and intermediate2[N] != sys.maxsize:
+    answer = start[mid1] + intermediate1[mid2] + intermediate2[N]
+if start[mid2] != sys.maxsize and intermediate2[mid1] != sys.maxsize and intermediate1[N] != sys.maxsize:
+    answer = min(answer, start[mid2] + intermediate2[mid1] + intermediate1[N])
+
+print(answer if answer != sys.maxsize else -1)
