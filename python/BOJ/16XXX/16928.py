b7d2216f1e78e09aad348412e9cc4253c5e02508
raiders032 <nameks@naver.com>
2022-07-29 21:57:03 +0900

    https://www.acmicpc.net/problem/16928 16928.뱀과 사다리 게임 풀이3.100ms

--- python/BOJ/16XXX/16928.py
+++ python/BOJ/16XXX/16928.py
@@ -1,52 +1,41 @@
 """
 https://www.acmicpc.net/problem/16928
 16928.뱀과 사다리 게임
-실버2
-BFS
-풀이2.96ms
+풀이3.100ms
 """
 import sys
-from collections import deque
-
-
-def solve():
-    q = deque([0])
-    visited = [[0] * 10 for _ in range(10)]
-    visited[0][0] = 1
-
-    while q:
-        current_position = q.popleft()
-        current_position_x = current_position // 10
-        current_position_y = current_position % 10
-        if current_position == 99:
-            return visited[9][9] - 1
-
-        for dice_num in range(1, 7):
-            next_position = current_position + dice_num
-
-            if next_position >= 100 or visited[next_position // 10][next_position % 10]:
-                continue
-
-            visited[next_position // 10][next_position % 10] = visited[current_position_x][current_position_y] + 1
-
-            if board[next_position // 10][next_position % 10]:
-                next_position = board[next_position // 10][next_position % 10]
-                if visited[next_position // 10][next_position % 10]:
-                    continue
-                visited[next_position // 10][next_position % 10] = visited[current_position_x][current_position_y] + 1
-
-            q.append(next_position)
-    return -1
-
+import collections
 
 input = sys.stdin.readline
+
 N, M = map(int, input().split())
-board = [[0] * 10 for _ in range(10)]
+jump = dict()
+for _ in range(N + M):
+    start, end = map(int, input().split())
+    jump[start] = end
 
-for i in range(N + M):
-    x, y = map(int, input().split())
-    x -= 1
-    y -= 1
-    board[x // 10][x % 10] = y
+visited = [-1] * 101
+visited[1] = 0
+q = collections.deque()
+q.append(1)
+while q:
+    vertex = q.popleft()
 
-print(solve())
+    if vertex == 100:
+        print(visited[100])
+        break
+
+    for i in range(1, 7):
+        next_vertex = vertex + i
+        if next_vertex > 100 or visited[next_vertex] != -1:
+            continue
+
+        visited[next_vertex] = visited[vertex] + 1
+        q.append(next_vertex)
+
+        while next_vertex in jump:
+            q.pop()
+            if visited[jump[next_vertex]] == -1:
+                visited[jump[next_vertex]] = visited[vertex] + 1
+                q.append(jump[next_vertex])
+            next_vertex = jump[next_vertex]
