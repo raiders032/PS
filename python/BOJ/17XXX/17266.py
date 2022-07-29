32dc58541db45b886d2e5aea182fa1206f19dbec
raiders032 <nameks@naver.com>
2022-07-29 18:33:37 +0900

    https://www.acmicpc.net/problem/17266 17266.어두운 굴다리 풀이1.340ms

---
+++ python/BOJ/17XXX/17266.py
@@ -0,0 +1,35 @@
+"""
+https://www.acmicpc.net/problem/17266
+17266.어두운 굴다리
+풀이1.340ms
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+M = int(input())
+lights = list(map(int, input().split()))
+left = 1
+right = N
+answer = sys.maxsize
+
+while left <= right:
+    height = (left + right) // 2
+
+    is_valid = True
+    bright_side = 0
+    for position in lights:
+        if bright_side < position - height:
+            break
+        bright_side = position + height
+
+    if bright_side < N:
+        is_valid = False
+
+    if is_valid:
+        right = height - 1
+        answer = min(answer, height)
+    else:
+        left = height + 1
+
+print(answer)
