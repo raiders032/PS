e4d2b842125a6617eaeaffa066b804ba075b2672
raiders032 <nameks@naver.com>
2022-07-29 18:34:08 +0900

    https://www.acmicpc.net/problem/1992 1992.쿼드트리 풀이2.76ms

--- python/BOJ/1XXX/1992.py
+++ python/BOJ/1XXX/1992.py
@@ -1,36 +1,37 @@
 """
 https://www.acmicpc.net/problem/1992
 1992.쿼드트리
-실버1
-풀이1.80ms
+풀이2.76ms
 """
 import sys
 
 input = sys.stdin.readline
 
 
-def solve(x, y, length):
-    wb = board[x][y]
+def get_quad_tree(x, y, length):
+    target = board[x][y]
     is_valid = True
+
     for i in range(x, x + length):
         for j in range(y, y + length):
-            if board[i][j] != wb:
+            if target != board[i][j]:
                 is_valid = False
                 break
         if not is_valid:
             break
+
     if is_valid:
-        print(wb, end='')
+        print(target, end='')
     else:
-        print('(', end='')
         unit = length // 2
-        solve(x, y, unit)
-        solve(x, y + unit, unit)
-        solve(x + unit, y, unit)
-        solve(x + unit, y + unit, unit)
-        print(')', end='')
+        print("(", end='')
+        get_quad_tree(x, y, unit)
+        get_quad_tree(x, y + unit, unit)
+        get_quad_tree(x + unit, y, unit)
+        get_quad_tree(x + unit, y + unit, unit)
+        print(")", end='')
 
 
-N = int(input())
-board = [list(map(int, input().rstrip())) for _ in range(N)]
-solve(0, 0, N)
+n = int(input())
+board = [list(map(int, input().rstrip())) for _ in range(n)]
+get_quad_tree(0, 0, n)
