4a461f32faa000d43b39d59855251da9f2b48421
raiders032 <nameks@naver.com>
2022-08-05 09:11:01 +0900

    https://www.acmicpc.net/problem/19948 19948.음유시인 영재 풀이1.72ms

--- python/BOJ/11XXX/11404.py
+++ python/BOJ/11XXX/11404.py
@@ -1,29 +1,32 @@
 """
 https://www.acmicpc.net/problem/11404
 11404.플로이드
-골드4
-풀이1.1000ms
+풀이2.704ms
 """
 import sys
 
 input = sys.stdin.readline
-INF = sys.maxsize
-V = int(input())
-E = int(input())
-dist = [[INF] * V for _ in range(V)]
+N = int(input())
+M = int(input())
+graph = [list() for _ in range(N)]
+dist = [[sys.maxsize] * N for _ in range(N)]  # 무한으로 인접행렬 초기화
 
-for _ in range(E):
-    v, w, d = map(int, input().split())
-    dist[v - 1][w - 1] = min(dist[v - 1][w - 1], d)
+for i in range(N):
+    dist[i][i] = 0
 
-for k in range(V):
-    for i in range(V):
-        for j in range(V):
-            if i == j:
-                continue
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    dist[a - 1][b - 1] = min(dist[a - 1][b - 1], c)
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
             dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
 
-for i in range(V):
-    for num in dist[i]:
-        print(num if num != INF else 0, end=' ')
+for i in range(N):
+    for d in dist[i]:
+        if d == sys.maxsize:
+            print(0, end=' ')
+        else:
+            print(d, end=' ')
     print()
