1ffff1ab57d8f602063e2c4bd5dd10fe379b7a75
raiders032 <nameks@naver.com>
2022-08-13 15:55:54 +0900

    https://www.acmicpc.net/problem/1932 1932.정수 삼각형 풀이2.240ms

--- python/BOJ/1XXX/1932.py
+++ python/BOJ/1XXX/1932.py
@@ -1,20 +1,18 @@
 """
+https://www.acmicpc.net/problem/1932
 1932.정수 삼각형
-실버1
-다이나믹 프로그래밍
+풀이2.240ms
 """
 import sys
 
 input = sys.stdin.readline
+
 N = int(input())
-board = []
+triangle = [list(map(int, input().split())) for _ in range(N)]
 dp = [[0] * N for _ in range(N)]
-for _ in range(N):
-    row = list(map(int, input().rstrip().split()))
-    board.append(row)
-dp[0][0] = board[0][0]
-for row in range(N - 1):
-    for col in range(row + 1):
-        dp[row + 1][col] = max(dp[row + 1][col], dp[row][col] + board[row + 1][col])
-        dp[row + 1][col + 1] = max(dp[row + 1][col + 1], dp[row][col] + board[row + 1][col + 1])
+dp[0][0] = triangle[0][0]
+for i in range(N - 1):
+    for j in range(i + 1):
+        dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
+        dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + triangle[i + 1][j + 1])
 print(max(dp[N - 1]))
