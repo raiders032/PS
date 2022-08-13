68fe60406da536c32682641345e551011a7cb100
raiders032 <nameks@naver.com>
2022-08-13 15:56:12 +0900

    https://www.acmicpc.net/problem/9251 9251.LCS 풀이1.688ms

---
+++ python/BOJ/9XXX/9251.py
@@ -0,0 +1,18 @@
+"""
+https://www.acmicpc.net/problem/9251
+9251.LCS
+풀이1.688ms
+"""
+import sys
+input = sys.stdin.readline
+string1 = input().rstrip()
+string2 = input().rstrip()
+dp = [[0] * (len(string2) + 1) for _ in range(len(string1) + 1)]
+for i in range(1, len(string1) + 1):
+    for j in range(1, len(string2) + 1):
+        if string1[i - 1] == string2[j - 1]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[len(string1)][len(string2)])
