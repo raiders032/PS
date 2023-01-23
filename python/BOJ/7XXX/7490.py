8959f1c1bc64651014a6779bafbc891f5e41ec5a
raiders032 <nameks@naver.com>
2023-01-23 19:49:17 +0900

    https://www.acmicpc.net/problem/7490 7490.0 만들기 풀이2.132ms

--- python/BOJ/7XXX/7490.py
+++ python/BOJ/7XXX/7490.py
@@ -1,24 +1,31 @@
 """
 https://www.acmicpc.net/problem/7490
 7490.0 만들기
-풀이1.160ms
+풀이2.132ms
 """
 import sys
+
 input = sys.stdin.readline
+answer = []
 
 
-def dfs(level, exp):
-    if level == N:
-        if eval(exp.replace(" ", "")) == 0:
-            print(exp)
+def dfs(level, expression):
+    if level == n:
+        if eval(expression.replace(" ", "")) != 0:
+            return
+
+        answer.append(expression)
+        answer.append('\n')
         return
 
-    dfs(level + 1, exp + " " + str(level + 1))
-    dfs(level + 1, exp + "+" + str(level + 1))
-    dfs(level + 1, exp + "-" + str(level + 1))
+    dfs(level + 1, expression + " " + str(level + 1))
+    dfs(level + 1, expression + "+" + str(level + 1))
+    dfs(level + 1, expression + "-" + str(level + 1))
 
 
 for _ in range(int(input())):
-    N = int(input())
+    n = int(input())
     dfs(1, "1")
-    print()
+    answer.append('\n')
+
+print(''.join(answer))
