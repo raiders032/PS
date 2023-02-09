57491985b7b39f4288afb51f184fa3b83642625d
raiders032 <nameks@naver.com>
2023-02-09 21:29:45 +0900

    https://www.acmicpc.net/problem/16935 16935.배열 돌리기 3 풀이1.924ms

--- python/BOJ/16XXX/16935.py
+++ python/BOJ/16XXX/16935.py
@@ -1,71 +1,72 @@
 """
+https://www.acmicpc.net/problem/16935
 16935.배열 돌리기 3
-실버3
+풀이1.924ms
 """
 import sys
-from collections import deque
 
 input = sys.stdin.readline
-N, M, K = map(int, input().split())
-board = [list(map(int, input().split())) for _ in range(N)]
 
+n, m, r = map(int, input().split())
+board = [list(map(int, input().split())) for _ in range(n)]
 
-def operation(op_num):
-    global board, N, M
-    tmp_board = deque()
-    if op_num == 1:
-        for row in board:
-            tmp_board.appendleft(row)
-    elif op_num == 2:
-        for row in board:
-            tmp_board.append(row[::-1])
-    elif op_num == 3:
-        tmp_board = [[] for _ in range(M)]
-        for r in range(N - 1, -1, -1):
-            for c in range(M):
-                tmp_board[c].append(board[r][c])
-        N, M = M, N
-    elif op_num == 4:
-        tmp_board = [[] for _ in range(M)]
-        for r in range(N):
-            for c in range(M - 1, -1, -1):
-                tmp_board[M - c - 1].append(board[r][c])
-        N, M = M, N
-    elif op_num == 5:
-        tmp_board = [[] for _ in range(N)]
-        for r in range(N // 2, N):
-            for c in range(0, M // 2):
-                tmp_board[r - N // 2].append(board[r][c])
-        for r in range(0, N // 2):
-            for c in range(0, M // 2):
-                tmp_board[r].append(board[r][c])
-        for r in range(N // 2, N):
-            for c in range(M // 2, M):
-                tmp_board[r].append(board[r][c])
-        for r in range(0, N // 2):
-            for c in range(M // 2, M):
-                tmp_board[r + N // 2].append(board[r][c])
-    elif op_num == 6:
-        tmp_board = [[] for _ in range(N)]
-        for r in range(0, N // 2):
-            for c in range(M // 2, M):
-                tmp_board[r].append(board[r][c])
-        for r in range(N // 2, N):
-            for c in range(M // 2, M):
-                tmp_board[r - N // 2].append(board[r][c])
-        for r in range(0, N // 2):
-            for c in range(0, M // 2):
-                tmp_board[r + N // 2].append(board[r][c])
-        for r in range(N // 2, N):
-            for c in range(0, M // 2):
-                tmp_board[r].append(board[r][c])
-    board = tmp_board
+for command in input().rstrip().split():
+    if command == '1':
+        board = board[::-1]
 
+    elif command == '2':
+        board = [row[::-1] for row in board]
 
-op_list = list(map(int, input().split()))
-for op in op_list:
-    operation(op)
+    elif command == '3':
+        board = list(map(list, map(reversed, zip(*board))))
+        n, m = m, n
+
+    elif command == '4':
+        board = list(map(list, zip(*board)))
+        board = board[::-1]
+        n, m = m, n
+
+    elif command == '5':
+        temp_board = [[0] * m for _ in range(n)]
+
+        for i in range(n // 2):
+            for j in range(m // 2):
+                temp_board[i][j + m // 2] = board[i][j]
+
+        for i in range(n // 2):
+            for j in range(m // 2, m):
+                temp_board[i + n // 2][j] = board[i][j]
+
+        for i in range(n // 2, n):
+            for j in range(m // 2, m):
+                temp_board[i][j - m // 2] = board[i][j]
+
+        for i in range(n // 2, n):
+            for j in range(m // 2):
+                temp_board[i - n // 2][j] = board[i][j]
+
+        board = temp_board
+
+    elif command == '6':
+        temp_board = [[0] * m for _ in range(n)]
+
+        for i in range(n // 2):
+            for j in range(m // 2):
+                temp_board[i + n // 2][j] = board[i][j]
+
+        for i in range(n // 2, n):
+            for j in range(m // 2):
+                temp_board[i][j + m // 2] = board[i][j]
+
+        for i in range(n // 2, n):
+            for j in range(m // 2, m):
+                temp_board[i - n // 2][j] = board[i][j]
+
+        for i in range(n // 2):
+            for j in range(m // 2, m):
+                temp_board[i][j - m // 2] = board[i][j]
+
+        board = temp_board
+
 for row in board:
-    for item in row:
-        print(item, end=' ')
-    print()
+    print(' '.join(map(str, row)))
