94f337d4837573adb2b38585604fda10638a6bca
raiders032 <nameks@naver.com>
2023-02-09 21:30:30 +0900

    7795.먹을 것인가 먹힐 것인가 https://www.acmicpc.net/problem/7795 풀이1.696ms

--- kotlin/src/main/kotlin/boj/7000/7795.kt
+++ kotlin/src/main/kotlin/boj/7000/7795.kt
@@ -2,24 +2,24 @@ package boj.`7000`
 
 import java.lang.StringBuilder
 
-/**
- * 7795.먹을 것인가 먹힐 것인가
- * https://www.acmicpc.net/problem/7795
- * 풀이1.708ms
- */
+/*
+7795.먹을 것인가 먹힐 것인가
+https://www.acmicpc.net/problem/7795
+풀이1.696ms
+*/
 fun main() {
-    val n = readln().toInt()
     val answer = StringBuilder("")
 
-    repeat(n) {
+    repeat(readln().toInt()) {
         var count = 0
         val N = readln()
         val A = readln().split(" ").map { it.toInt() }.toList()
         val B = readln().split(" ").map { it.toInt() }.toList().sorted()
 
-        for (i in 0 until A.size) {
-            count += biSearch(B, A[i])
+        repeat(A.size) {
+            count += biSearch(B, A[it])
         }
+
         answer
             .append(count)
             .append("\n")
