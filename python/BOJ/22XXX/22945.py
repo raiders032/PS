4ce3845eda9141a2646909e8bd5d3d1ce198aaa5
raiders032 <nameks@naver.com>
2023-05-17 21:00:38 +0900

    https://www.acmicpc.net/problem/22945 22945.팀 빌딩 풀이2.156ms

--- python/BOJ/22XXX/22945.py
+++ python/BOJ/22XXX/22945.py
@@ -1,23 +1,24 @@
 """
 https://www.acmicpc.net/problem/22945
 22945.팀 빌딩
-풀이1.140ms
+풀이2.156ms
 """
 import sys
 input = sys.stdin.readline
 
 n = int(input())
-developers = list(map(int, input().split()))
-left = 0
-right = n - 1
-answer = 0
+score = list(map(int, input().split()))
+low = 0
+high = n - 1
+answer = min(score[low], score[high]) * (high - low - 1)
 
-while left != right:
-    answer = max(answer, (right - left - 1) * min(developers[left], developers[right]))
+while low < high:
+    team_score = min(score[low], score[high]) * (high - low - 1)
+    answer = max(answer, team_score)
 
-    if developers[left] <= developers[right]:
-        left += 1
+    if score[low] <= score[high]:
+        low += 1
     else:
-        right -= 1
+        high -= 1
 
 print(answer)
