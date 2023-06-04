1308f2e3ca9452f85cb73daab61b8e2880411c5d
raiders032 <nameks@naver.com>
2023-06-04 10:37:36 +0900

    https://www.acmicpc.net/problem/22856 22856.트리 순회 풀이2.296ms

--- python/BOJ/22XXX/22856.py
+++ python/BOJ/22XXX/22856.py
@@ -1,25 +1,39 @@
 """
 https://www.acmicpc.net/problem/22856
 22856.트리 순회
-풀이1.200ms
+풀이2.296ms
 """
 import sys
-
-input = sys.stdin.readline
 sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
 
 
-def get_depth(node):
+def traversal_all(node):
+    global count
+    if tree[node][0] != -1:
+        count += 2
+        traversal_all(tree[node][0])
+
     if tree[node][1] != -1:
-        return 1 + get_depth(tree[node][1])
-    return 1
+        count += 2
+        traversal_all(tree[node][1])
+
+
+def traversal_right(node):
+    global count
+
+    if tree[node][1] != -1:
+        count -= 1
+        traversal_right(tree[node][1])
 
 
 n = int(input())
-tree = [0] * (n + 1)
-
+tree = dict()
 for _ in range(n):
     parent, left_child, right_child = map(int, input().split())
     tree[parent] = (left_child, right_child)
 
-print(2 * n - get_depth(1) - 1)
+count = 0
+traversal_all(1)
+traversal_right(1)
+print(count)
